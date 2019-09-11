3066e3cdd45c8cf2e825fcf824a5960f40ab1a9c
Henrique de Castilhos <henrique.castilhos@softexpert.com>
2019-09-11 15:36:23 -0300

    :sparkles: Added router links in header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,12 +2,25 @@
   <nav class="navbar" role="navigation">
     <div class="container">
       <div class="navbar-brand">
-        <img
-          src="https://hacktoberfest.digitalocean.com/HF19_logo.png"
-          alt="Hacktoberfest 2019"
-          class="hacktoberfest-logo"
-          width="449"
-        />
+        <router-link to="/">
+          <img
+            src="https://hacktoberfest.digitalocean.com/HF19_logo.png"
+            alt="Hacktoberfest 2019"
+            class="hacktoberfest-logo"
+            width="449"
+          />
+        </router-link>
+      </div>
+
+      <div class="navbar-menu is-active">
+        <div class="navbar-end">
+          <router-link
+            v-for="route in routes"
+            :key="route.name"
+            :to="route.path"
+            class="route"
+          >{{ route.name }}</router-link>
+        </div>
       </div>
     </div>
   </nav>
@@ -16,6 +29,13 @@
 <script>
 export default {
   name: 'Header',
+  data: () => ({
+    routes: [
+      { path: '/catolica', name: 'Católica' },
+      { path: '/fablab', name: 'FabLab' },
+      { path: '/udesc', name: 'Udesc' },
+    ],
+  }),
 };
 </script>
 
@@ -33,6 +53,32 @@ export default {
 .hacktoberfest-logo {
   height: auto;
   max-height: none !important;
+}
+
+.route {
+  margin-left: 2.25rem;
+  font-weight: bold;
+  font-size: 14px;
+  height: 30px;
+  line-height: 21px;
+  -webkit-letter-spacing: 3px;
+  -moz-letter-spacing: 3px;
+  -ms-letter-spacing: 3px;
+  letter-spacing: 3px;
+  text-transform: uppercase;
+  color: #fff;
+  margin-right: 30px;
+}
+
+.route:hover {
+  color: #fff;
+  border-bottom: 2px solid #fff922;
+}
+
+@media (max-width: 1087px) {
+  .navbar-end {
+    text-align: center;
+  }
 }
 
 @media (max-width: 1087px) {
