19d278aefc4a11b735d25998a92bf164f57065e4
Henrique de Castilhos <henrique.castilhos@softexpert.com>
2019-09-11 12:00:36 -0300

    :sparkles: Created Header component with the Hacktoberfest '19 logo

---
+++ src/components/Header.vue
@@ -0,0 +1,44 @@
+<template>
+  <nav class="navbar" role="navigation">
+    <div class="container">
+      <div class="navbar-brand">
+        <img
+          src="https://hacktoberfest.digitalocean.com/HF19_logo.png"
+          alt="Hacktoberfest 2019"
+          class="hacktoberfest-logo"
+          width="449"
+        />
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+};
+</script>
+
+<style lang="scss" scoped>
+.navbar {
+  background-color: #152347;
+  min-height: 140px;
+}
+
+.navbar-menu {
+  background-color: rgb(255, 255, 255, 0);
+  box-shadow: none;
+}
+
+.hacktoberfest-logo {
+  height: auto;
+  max-height: none !important;
+}
+
+@media (max-width: 1087px) {
+  .navbar-brand {
+    display: block;
+    text-align: center;
+  }
+}
+</style>
