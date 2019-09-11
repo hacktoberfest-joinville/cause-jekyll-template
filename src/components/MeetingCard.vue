8b5c88936a80385594923ef029c5f8601158b890
Henrique de Castilhos <henrique.castilhos@softexpert.com>
2019-09-11 17:42:54 -0300

    :construction: Creating MeetingCard

--- src/components/MeetingCard.vue
+++ src/components/MeetingCard.vue
@@ -1,6 +1,18 @@
 <template>
   <!-- TODO: criar o card com os detalhes do evento -->
-  <div>{{meeting}}</div>
+  <div class="meeting-card">
+    <h1 class="title">
+      {{ meeting.title }}
+    </h1>
+
+    <div class="body">
+      {{meeting}}
+
+      <div class="actions">
+        <button class="action-button">Saber mais</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -13,4 +25,55 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.meeting-card {
+  width: 100%;
+  background-color: #39486f;
+  vertical-align: top;
+  padding: 0;
+}
+
+.meeting-card > .title {
+  padding: 15px;
+  text-align: center;
+  color: #fff;
+  font-weight: bold;
+  font-size: 18px;
+  line-height: 27px;
+  display: block;
+  width: 100%;
+  border: none;
+  background-color: #1f2d4f
+}
+
+.meeting-card > .body > .actions {
+  display: block;
+  text-align: center;
+  width: 100%;
+  padding-bottom: 15px;
+}
+
+.meeting-card > .body > .actions > .action-button {
+  border: none;
+  color: #fff;
+  cursor: pointer;
+  height: 40px;
+  width: 160px;
+  background-color: #a11ec6;
+  font-weight: bold;
+  font-size: 14px;
+}
+.meeting-card > .body > .actions > .action-button:hover {
+  color: #39486e;
+  background-color: #92eaff;
+}
+@media (max-width: 1024px) {
+  .meeting-card > .body > .actions > .action-button {
+    font-size: 14px;
+  }
+}
+@media (max-width: 768px) {
+  .meeting-card > .body > .actions > .action-button {
+    font-size: 11px;
+  }
+}
 </style>
