2630ed237d863b6cf03a3fabdbe59ce93a4a96c2
Henrique de Castilhos <henrique.castilhos@softexpert.com>
2019-09-11 13:35:46 -0300

    :construction: Creating Description component

---
+++ src/components/Description.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="hero-body">
+    <div class="container">
+      <div class="content">
+        <h1 class="page-title">Hacktoberfest Joinville - 2019</h1>
+
+        <h2 id="whats-hacktoberfest" class="page-subtitle">O que é a Hacktoberfest?</h2>
+        <p class="description">
+          Lorem ipsun dolor sit amet
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Description',
+};
+</script>
+
+<style lang="scss" scoped>
+.hero-body {
+  display: block;
+  -webkit-align-items: top;
+  -webkit-box-align: top;
+  -ms-flex-align: top;
+  align-items: top;
+}
+
+.page-title {
+  font-size: 3em;
+  font-weight: bold;
+  color: #fff922;
+  text-align: center;
+}
+
+.page-subtitle {
+  font-size: 2em;
+  font-weight: bold;
+  color: #ff00aa;
+  border-bottom: 1px solid #ff00aa;
+  padding: 30px 0 15px 0;
+}
+
+p.description {
+  color: #aab2c0;
+}
+</style>
