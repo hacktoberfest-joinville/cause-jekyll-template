3d187cf68c8db75516086cc52707025ac6a54016
Henrique de Castilhos <henrique.castilhos@softexpert.com>
2019-09-11 16:40:27 -0300

    :heavy_plus_sign: Vue Magic Grid added to project

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue';
+import MagicGrid from 'vue-magic-grid';
 import App from './App.vue';
 import router from './router';
 
 Vue.config.productionTip = false;
+
+Vue.use(MagicGrid);
 
 new Vue({
   router,
