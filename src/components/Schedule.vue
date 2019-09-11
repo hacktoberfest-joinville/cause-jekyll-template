fe552574f98ee7f1f6ef13efc8f9795f7699f78f
Henrique de Castilhos <henrique.castilhos@softexpert.com>
2019-09-11 15:27:01 -0300

    :construction: Creating Schedule component

---
+++ src/components/Schedule.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="hero-body">
+    <div class="container">
+      <div class="content">
+        <h2 id="whats-hacktoberfest" class="page-subtitle">Agenda</h2>
+        <p class="description">
+          Adicionar agenda (baseada num JSON)
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Schedule',
+};
+</script>
+
+
+<style lang="scss" scoped>
+.hero-body {
+  display: block;
+  -webkit-align-items: top;
+  -webkit-box-align: top;
+  -ms-flex-align: top;
+  align-items: top;
+}
+
+.page-title {
+  font-size: 2.5em;
+  font-weight: bold;
+  color: #fff922;
+  text-align: center;
+}
+
+.page-subtitle {
+  font-size: 2em;
+  font-weight: bold;
+  color: #ff00aa;
+  border-bottom: 1px solid #ff00aa;
+  padding: 30px 0 15px 0;
+}
+
+p.description {
+  color: #aab2c0;
+}
+</style>
