af65bbd97a37c223a8411bc33ea59533ddaa855e
Alan Fachini <4171+alfakini@users.noreply.github.com>
2020-09-02 19:32:55 -0300

    Update Description.vue

--- src/components/Description.vue
+++ src/components/Description.vue
@@ -6,12 +6,11 @@
 
         <h2 id="whats-hacktoberfest" class="page-subtitle">O que é a Hacktoberfest?</h2>
         <p class="description">
-          Hacktoberfest — trago à você pela DigitalOcean em parceria com o DEV — é uma celebração
-          ao open source que ocorre anualmente durante o mês de outubro. Os mantenedores são
-          convidados a guiar os colaboradores em questões que ajudarão no avanço do projeto, e os
-          colaboradores têm a oportunidade de ajudar os projetos que eles gostam ou que acabaram de
-          conhecer. Nenhuma contribuição é pequena demais — correções de bugs e ajustes na
-          documentação são formas válidas de participar e ajudar a comunidade.
+          Hacktoberfest é um evento mundial liderado pela DigitalOcean em parceria com o DEV — para celebrar
+          o open source  — que ocorre anualmente durante o mês de outubro. Os mantenedores são
+          convidados a guiar os colaboradores no desenvolvimento de issues que ajudarão na evolução de projetos open source, e os
+          colaboradores têm a oportunidade de ajudar os projetos que eles gostam. Nenhuma contribuição é pequena, correções de bugs e ajustes na
+          documentação são formas válidas de participar e ajudar a comunidade!
         </p>
       </div>
     </div>
