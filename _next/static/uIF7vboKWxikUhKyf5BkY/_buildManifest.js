0e2793d02b2ccb23b84df89157ec76594d86d1c2
Henrique de Castilhos <henrique@castilhos.dev>
2020-09-18 17:14:30 -0300

    Updates

--- _next/static/uIF7vboKWxikUhKyf5BkY/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {"/":["static\u002Fchunks\u002Fpages\u002Findex-74a56f2a4ed01a3c1f11.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-4f0601949c71cd429f69.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
